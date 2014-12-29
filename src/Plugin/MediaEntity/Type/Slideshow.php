603f281b58161716a0b1b7dd179f375f8593b951
Janez Urevc <janez@janezurevc.name>
2014-12-29 15:26:12 +0100

    by slashrsm: Add validation.

--- src/Plugin/MediaEntity/Type/Slideshow.php
+++ src/Plugin/MediaEntity/Type/Slideshow.php
@@ -10,6 +10,7 @@ namespace Drupal\media_entity_slideshow\Plugin\MediaEntity\Type;
 use Drupal\media_entity\MediaBundleInterface;
 use Drupal\media_entity\MediaInterface;
 use Drupal\media_entity\MediaTypeBase;
+use Drupal\media_entity\MediaTypeException;
 
 /**
  * Provides media type plugin for Slideshows.
@@ -55,9 +56,13 @@ class Slideshow extends MediaTypeBase {
 
     $options = array();
     $allowed_field_types = array('entity_reference');
+    /** @var \Drupal\Core\Field\FieldDefinitionInterface $field */
     foreach ($this->entityManager->getFieldDefinitions('media', $bundle->id()) as $field_name => $field) {
-      if (in_array($field->getType(), $allowed_field_types) && !$field->getFieldStorageDefinition()->isBaseField()) {
-        $options[$field_name] = $field->getLabel();
+      if (in_array($field->getType(), $allowed_field_types)) {
+        $storage = $field->getFieldStorageDefinition();
+        if (!$storage->isBaseField() && $storage->getSetting('target_type') == 'media') {
+          $options[$field_name] = $field->getLabel();
+        }
       }
     }
 
@@ -76,9 +81,26 @@ class Slideshow extends MediaTypeBase {
    * {@inheritdoc}
    */
   public function validate(MediaInterface $media) {
-    // This should be handled by Drupal core.
-    // - check if there is at least one slideshow item
-    // - check if all items are media (entity reference references media)
+    $source_field = $this->configuration['source_field'];
+    $field_definitions = $this->entityManager->getFieldDefinitions('media', $media->bundle());
+    /** @var \Drupal\Core\Field\FieldDefinitionInterface $source_field_definition */
+    $source_field_definition = $field_definitions[$source_field];
+
+    // Validate slideshow items count.
+    if ($media->{$source_field}->isEmpty()) {
+      throw new MediaTypeException($this->configuration['source_field'], 'At least one slideshow item must exist.');
+    }
+
+    // Validate slideshow items bundles.
+    $handler_settings = $source_field_definition->getSetting('handler_settings');
+    if (!empty($handler_settings['target_bundles'])) {
+      /** @var \Drupal\Core\Entity\EntityInterface $entity */
+      foreach ($media->{$source_field}->referencedEntities() as $entity) {
+        if (!in_array($entity->bundle(), $handler_settings['target_bundles'])) {
+          throw new MediaTypeException($this->configuration['source_field'], 'Slideshow item of incorrect media bundle detected.');
+        }
+      }
+    }
   }
 
   /**
@@ -89,20 +111,17 @@ class Slideshow extends MediaTypeBase {
 
     /** @var \Drupal\media_entity\MediaInterface $slideshow_item */
     $slideshow_item = $this->entityManager->getStorage('media')->load($media->{$source_field}->target_id);
-
     if (!$slideshow_item) {
       return $this->config->get('icon_base') . '/slideshow.png';
     }
 
     /** @var \Drupal\media_entity\MediaBundleInterface $bundle */
     $bundle = $this->entityManager->getStorage('media_bundle')->load($slideshow_item->bundle());
-
     if (!$bundle) {
       return $this->config->get('icon_base') . '/slideshow.png';
     }
 
     $thumbnail = $bundle->getType()->thumbnail($media);
-
     if (!$thumbnail) {
       return $this->config->get('icon_base') . '/slideshow.png';
     }
